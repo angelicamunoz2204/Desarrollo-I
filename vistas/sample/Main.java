4ada4b625bf979be1231f4d93837c2eb5eda3d6a
Super Pulpita <angelica.benavides@correounivalle.edu.co>
2020-02-23 18:40:24 -0500

    Mi primer commit

--- vistas/sample/Main.java
+++ vistas/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-
+        //--module-path ${PATH_TO_FX} --add-modules javafx.controls,javafx.fxml
 
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         Parent logged =FXMLLoader.load(getClass().getResource("sample.fxml"));
